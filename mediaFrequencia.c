e68f20ab1578f9ed3895935162e88ae544f9a01d
Paulo Thomaz <thomazfilhop@gmail.com>
2026-09-14 22:00:06 -0300

    media frequencia

---
+++ mediaFrequencia.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h> 
+
+int main() {
+    float n1, n2, m, frequencia;
+   
+    printf("Dígite a sua primeira nota: ");
+    scanf("%f", &n1);
+   
+    printf("Dígite a sua segunda nota: ");
+    scanf("%f", &n2);
+    
+    printf("Dígite a sua frequencia: ");
+    scanf("%f", &frequencia);
+    
+    m = (n1 + n2 ) / 2 ;
+    
+    if(frequencia < 75){
+        printf("Você não passou");
+    } else if(m > 6){
+        printf("Você passou");
+    } else {
+        printf("Você não passou");
+    }
+    return 0;
+}
